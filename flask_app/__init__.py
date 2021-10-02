e683457723f4b17e93ba05c158306ccc2ea35cdb
Silicrex <silicrex@gmail.com>
2021-10-02 14:31:41 -0400

    Index route

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,1 +1,5 @@
 from flask import Flask
+
+app = Flask(__name__)  # Flask app object
+
+from flask_app import routes  # Below app definition to avoid circular import. Routes needs app
---
+++ main.py
@@ -0,0 +1,4 @@
+from flask_app import app  # Get main Flask object
+
+if __name__ == '__main__':
+    app.run(debug=True)
