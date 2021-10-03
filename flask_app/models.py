300759248cebe3f93592d6c8475b8245b6abb7cd
Silicrex <silicrex@gmail.com>
2021-10-02 22:47:53 -0400

    Password hashing

--- flask_app/models.py
+++ flask_app/models.py
@@ -1,13 +1,34 @@
 from flask_app import db
+from datetime import datetime as Datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 # Columns are fields
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)  # User account id
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    # backref for name for connected object. post.author will give user
 
     def __repr__(self):  # For printing/debugging
         return '<User {}>'.format(self.username)
 
+    def set_password(self, password):  # Cryptographically hash password
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):  # Validate password
+        return check_password_hash(self.password_hash, password)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=Datetime.utcnow)  # Function call given
+    password_hash = db.Column(db.String(128))
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from flask_app import app
+from flask_app import app, db
+from flask_app.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():  # For flask shell
+    return {'db': db, 'User': User, 'Post': Post}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
