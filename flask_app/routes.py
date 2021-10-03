49b91956a987346a68f24a8eb03d6977c59b9922
Silicrex <silicrex@gmail.com>
2021-10-02 20:27:40 -0400

    Mock login form

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,7 +1,18 @@
 from flask_app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from flask_app.forms import LoginForm
+
 
 @app.route('/')
 def index():
     user = {'username: Admin'}
     return render_template('index.html', title='Home', user=user)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login success')
+        return redirect(url_for('index'))
+    return render_template('login.html', titlw='Sign In', form=form)
