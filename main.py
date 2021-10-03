68c6a3add74979892bda7cf82aa86401d21535c1
Silicrex <silicrex@gmail.com>
2021-10-03 18:48:41 -0400

    Remove old reference to deprecated Post schema

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask_app.models import (User, PhysicalSubmission, MentalSubmission, Hazard
 
 @app.shell_context_processor
 def make_shell_context():  # For flask shell
-    return {'db': db, 'User': User, 'Post': Post, 'PhysicalSubmission': PhysicalSubmission,
+    return {'db': db, 'User': User, 'PhysicalSubmission': PhysicalSubmission,
             "MentalSubmission": MentalSubmission, "HazardSubmission": HazardSubmission,
             "DiversitySubmission": DiversitySubmission, "ResourceSubmission": ResourceSubmission,
             "MiscSubmission": MiscSubmission, "Resources": Resources}
